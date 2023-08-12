ae9cdb07d9d187038b8dab20b595edd2807c4e04
fancilivain <meigangwen@gmail.com>
2023-08-12 22:52:46 +0800

    can load a model using three.js

--- app/demo2/Map.tsx
+++ app/demo2/Map.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react'
 import maplibregl from 'maplibre-gl'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function Map(props) {
     
@@ -39,8 +41,85 @@ export default function Map(props) {
             scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits()
         }
 
-        
+        // configuration of the custom layer for a 3D model per the CustomLayerInterface
+        const customLayer = {
+            id: '3d-model',
+            type: 'custom',
+            renderingMode: '3d',
+            onAdd (map, gl) {
+                this.camera = new THREE.Camera()
+                this.scene = new THREE.Scene()
 
+                // create two three.js lights to illuminate the model
+                const directionalLight = new THREE.DirectionalLight(0xffffff)
+                directionalLight.position.set(0, -70, 100).normalize()
+                this.scene.add(directionalLight)
+
+                const directionalLight2 = new THREE.DirectionalLight(0xffffff)
+                directionalLight2.position.set(0, 70, 100).normalize()
+                this.scene.add(directionalLight2)
+
+                // use the three.js GLTF loader to add the 3D model to the three.js scene
+                const loader = new GLTFLoader()
+                loader.load(
+                    'https://maplibre.org/maplibre-gl-js/docs/assets/34M_17/34M_17.gltf',
+                    (gltf) => {
+                        this.scene.add(gltf.scene)
+                    }
+                );
+                this.map = map
+
+                // use the MapLibre GL JS map canvas for three.js
+                this.renderer = new THREE.WebGLRenderer({
+                    canvas: map.getCanvas(),
+                    context: gl,
+                    antialias: true
+                })
+
+                this.renderer.autoClear = false
+            },
+            render (gl, matrix) {
+                const rotationX = new THREE.Matrix4().makeRotationAxis(
+                    new THREE.Vector3(1, 0, 0),
+                    modelTransform.rotateX
+                )
+                const rotationY = new THREE.Matrix4().makeRotationAxis(
+                    new THREE.Vector3(0, 1, 0),
+                    modelTransform.rotateY
+                )
+                const rotationZ = new THREE.Matrix4().makeRotationAxis(
+                    new THREE.Vector3(0, 0, 1),
+                    modelTransform.rotateZ
+                )
+
+                const m = new THREE.Matrix4().fromArray(matrix);
+                const l = new THREE.Matrix4()
+                    .makeTranslation(
+                        modelTransform.translateX,
+                        modelTransform.translateY,
+                        modelTransform.translateZ
+                    )
+                    .scale(
+                        new THREE.Vector3(
+                            modelTransform.scale,
+                            -modelTransform.scale,
+                            modelTransform.scale
+                        )
+                    )
+                    .multiply(rotationX)
+                    .multiply(rotationY)
+                    .multiply(rotationZ)
+
+                this.camera.projectionMatrix = m.multiply(l)
+                this.renderer.resetState()
+                this.renderer.render(this.scene, this.camera)
+                this.map.triggerRepaint()
+            }
+        }
+
+        map.on('style.load', () => {
+            map.addLayer(customLayer);
+        })
       }, [])
 
     return (
